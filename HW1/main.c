00e6fc7f1e686385a48205bf2926697c970ec463
anotherjoshsmith <jsmith07@uw.edu>
2017-01-24 13:46:24 -0800

    Initialize.

---
+++ HW1/main.c
@@ -0,0 +1,9 @@
+//
+//  main.c
+//  
+//
+//  Created by Joshua Smith on 1/12/17.
+//
+//
+
+#include "main.h"
---
+++ HW1/main.h
@@ -0,0 +1,14 @@
+//
+//  main.h
+//  
+//
+//  Created by Joshua Smith on 1/12/17.
+//
+//
+
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
+#endif /* main_h */
